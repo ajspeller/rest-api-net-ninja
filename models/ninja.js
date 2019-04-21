44b3d80d03eed8aed514b42cd196da45c27ce44b
AJ Speller <ajspeller@gmail.com>
2019-04-20 21:32:18 -0400

    Add geolocation schema

--- models/ninja.js
+++ models/ninja.js
@@ -2,6 +2,18 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 
+// create geolocation schema
+const GeoSchema = new Schema({
+  type: {
+    type: String,
+    default: 'Point'
+  },
+  coordinates: {
+    type: [Number],
+    index: '2dsphere'
+  }
+});
+
 // create ninja schema and model
 const NinjaSchema = new Schema({
   name: {
@@ -14,8 +26,8 @@ const NinjaSchema = new Schema({
   available: {
     type: Boolean,
     default: false
-  }
-  // TODO: add in geolocation
+  },
+  geometry: GeoSchema
 });
 
 const Ninja = mongoose.model('ninja', NinjaSchema);
